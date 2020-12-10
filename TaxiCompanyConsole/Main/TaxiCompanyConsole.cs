e831b803db89ac4778a347fa04266c8af14973ee
Meleshko Alexander <meleshko.alex.0@gmail.com>
2020-12-11 02:55:35 +0300

    Console client update 2

---
+++ TaxiCompanyConsole/Main/TaxiCompanyConsole.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TaxiCompany.Auto;
+using TaxiCompany.TaxiCompany;
+using TaxiCompanyConsole.UI;
+
+namespace TaxiCompanyConsole
+{
+    public class TaxiCompanyConsole
+    {
+        private readonly TaxiCompany<Car> _taxiCarCompany;
+        private readonly List<Car> _carList;
+        public TaxiCompanyConsole()
+        {
+            _taxiCarCompany = new TaxiCompany<Car>();
+            _taxiCarCompany.Load();
+            _carList = _taxiCarCompany.GetCopy();
+        }
+
+        public void Create()
+        {
+            var car = new Car { Name = UI.Ui.OutputMessage("Name"),
+                Consumption = Convert.ToDouble(Ui.OutputMessage("Consumption")),
+                Cost = Convert.ToDouble(UI.Ui.OutputMessage("Cost")),
+                Year = Convert.ToInt32(UI.Ui.OutputMessage("Year")),
+                StateNumber =UI.Ui.OutputMessage("State number"),
+                Vin = UI.Ui.OutputMessage("Vin number"),
+                MaxSpeed = Convert.ToDouble(UI.Ui.OutputMessage("Max Speed"))
+            };
+            _taxiCarCompany.Add(car);
+            _taxiCarCompany.Save();
+            Console.Clear();
+        }
+
+        public void Show()
+        {
+            foreach (var car in _carList)
+            {
+                Ui.InputMessage($"Name:{car.Name}");
+                Ui.InputMessage($"Year:{car.Year}");
+                Ui.InputMessage($"Cost:{car.Cost}");
+                Ui.InputMessage($"State number:{car.StateNumber}");
+                Ui.InputMessage($"VIN number:{car.Vin}");
+                Ui.InputMessage($"Max Speed:{car.MaxSpeed}");
+                Ui.InputMessage($"Consumption:{car.Consumption}");
+            }
+        }
+
+        public void Sort()
+        {
+            TaxiCompanyEconomic.Sort(_carList,(a,b)=>a.Consumption.CompareTo(b.Consumption));
+        }
+    }
+}
---
+++ TaxiCompanyConsole/UI/UI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TaxiCompanyConsole.UI
+{
+    public class Ui
+    {
+        public static void Menu()
+        {
+            var taxiConsole=new TaxiCompanyConsole();
+            while (true)
+            {
+                InputMenu();
+                var choose = Convert.ToInt32(OutputMessage(">>"));
+                switch (choose)
+                {
+                    default:throw new Exception("This number not exist in this menu");
+                    case 1:
+                    {
+                        taxiConsole.Show();
+                        break;
+                    }
+                    case 2:
+                    {
+                        taxiConsole.Create();
+                        break;
+                    }
+                    case 3:
+                    case 4:
+                    {
+                        taxiConsole.Sort();
+                        break;
+                    }
+                    case 0:
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+                }
+            }
+            
+        }
+
+        private static void InputMenu()
+        {
+            InputMessage("1.Show");
+            InputMessage("2.Create ");
+            InputMessage("3.Company cost");
+            InputMessage("4.Sort by Consumption");
+            InputMessage("5.Find by speed");
+            InputMessage("0. Exit");
+        }
+        public static void InputMessage(string mes)
+        {
+            Console.WriteLine(mes);
+        }
+
+        public static string OutputMessage(string mes = null)
+        {
+            Console.WriteLine(mes);
+            return Console.ReadLine();
+        }
+    }
+}
