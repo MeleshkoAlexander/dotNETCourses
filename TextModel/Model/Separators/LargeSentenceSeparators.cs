fced24c1063d669f5096a451aba2917cf6cf9eab
Meleshko Alexander <meleshko.alex.0@gmail.com>
2020-12-27 05:09:30 +0300

    Object model create

---
+++ TextModel/Model/Separators/LargeSentenceSeparators.cs
@@ -0,0 +1,14 @@
+﻿using TextModel.Interfaces;
+
+namespace TextModel.Model.Separators
+{
+    public class LargeSentenceSeparators : ISeparator
+    {
+        public string[] separators { get; }
+
+        public LargeSentenceSeparators()
+        {
+            separators = new[] {"...", "?!", "!?"};
+        }
+    }
+}
